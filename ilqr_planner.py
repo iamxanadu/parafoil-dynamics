a171b0a57fe0773044c5bf8e069da3326f19c6d2
Josef <jxb@mit.edu>
2020-04-21 12:27:24 -0400

    Ready to test ilqr

--- ilqr_planner.py
+++ ilqr_planner.py
@@ -1,39 +1,42 @@
-from dynamics import discrete_dynamics
 import numpy as np
-from sympy import lambdify
-
-from constants import n_x, n_u
 
 
 class ILQRPlanner():
 
-    def __init__(self, f, lx, lu, lxx, lux, luu, fx, fu):
-        self.xs = xs
-        self.u = u
+    def __init__(self, nx, nu, f, l, lf, lx, lu, lxx, lux, luu, fx, fu, dt):
         self.f = f
-        self.const_dict = const_dict
+        self.l = l
+        self.lf = lf
 
-        self.fx = self.f.jacobian(self.xs)
-        self.fu = self.f.jacobian(self.u)
+        self.lx = lx
+        self.lu = lu
+        self.lxx = lxx
+        self.lux = lux
+        self.luu = luu
+        self.fx = fx
+        self.fu = fu
 
-        self.func_f = lambdify((self.xs.tolist(), self.u.tolist()), self.f.subs(
-            self.const_dict), modules='numpy')
+        self.dt = dt
 
-        self.func_fx = lambdify(
-            (self.xs.tolist(), self.u.tolist()), self.fx.subs(self.const_dict), modules='numpy')
-
-        self.func_fu = lambdify(
-            (self.xs.tolist(), self.u.tolist()), self.fu.subs(self.const_dict), modules='numpy')
+        self.nx = nx
+        self.nu = nu
 
     def _discrete_dynamics(self, x, u, dt):
-        return self.func_f(x, u) * dt + x
+        return self.f(x, u) * dt + x
 
     def _ilqr_rollout(self, x0, u_trj):
         x_trj = np.zeros((u_trj.shape[0]+1, x0.shape[0]))
         x_trj[0] = x0
         for i in range(u_trj.shape[0]):
-            x_trj[i+1] = discrete_dynamics(x_trj[i], u_trj[i])
+            x_trj[i+1] = self._discrete_dynamics(x_trj[i], u_trj[i], self.dt)
         return x_trj
+
+    def _ilqr_cost_trj(self, x_trj, u_trj):
+        total = 0.0
+        for i in range(u_trj.shape[0]):
+            total += self.l(x_trj[i], u_trj[i])
+        total += self.lf(x_trj[-1])
+        return total
 
     def _ilqr_forward_pass(self, x_trj, u_trj, k_trj, K_trj):
         x_trj_new = np.zeros(x_trj.shape)
@@ -42,10 +45,11 @@ class ILQRPlanner():
         for n in range(u_trj.shape[0]):
             u_trj_new[n, :] = u_trj[n, :] + k_trj[n] + \
                 K_trj[n].dot(x_trj_new[n] - x_trj[n])
-            x_trj_new[n+1, :] = discrete_dynamics(x_trj_new[n], u_trj_new[n])
+            x_trj_new[n+1, :] = self._discrete_dynamics(
+                x_trj_new[n], u_trj_new[n], self.dt)
         return x_trj_new, u_trj_new
 
-    def _ilqr_backward_pass_quadratic_cost(self, x_trj, u_trj, Q, Qf, R, regu):
+    def _ilqr_backward_pass(self, x_trj, u_trj, regu):
         k_trj = np.zeros([u_trj.shape[0], u_trj.shape[1]])
         K_trj = np.zeros([u_trj.shape[0], u_trj.shape[1], x_trj.shape[1]])
         expected_cost_redu = 0
@@ -54,8 +58,8 @@ class ILQRPlanner():
         V_xx = np.zeros((x_trj.shape[1], x_trj.shape[1]))
         for n in range(u_trj.shape[0]-1, -1, -1):
             # TODO: First compute derivatives, then the Q-terms
-            l_x, l_u, l_xx, l_ux, l_uu, f_x, f_u = self._get_derivatives_ilqr_quadratic_cost(
-                x_trj[n], u_trj[n], Q, Qf, R)
+            l_x, l_u, l_xx, l_ux, l_uu, f_x, f_u = self._get_derivatives_ilqr(
+                x_trj[n], u_trj[n])
             Q_x, Q_u, Q_xx, Q_ux, Q_uu = self._ilqr_Q_terms(
                 l_x, l_u, l_xx, l_ux, l_uu, f_x, f_u, V_x, V_xx)
             # We add regularization to ensure that Q_uu is invertible and nicely conditioned
@@ -64,7 +68,8 @@ class ILQRPlanner():
             k_trj[n, :] = k
             K_trj[n, :, :] = K
             V_x, V_xx = self._ilqr_V_terms(Q_x, Q_u, Q_xx, Q_ux, Q_uu, K, k)
-            expected_cost_redu += expected_cost_reduction(Q_u, Q_uu, k)
+            expected_cost_redu += self._ilqr_expected_cost_reduction(
+                Q_u, Q_uu, k)
         return k_trj, K_trj, expected_cost_redu
 
     def _ilqr_Q_terms(l_x, l_u, l_xx, l_ux, l_uu, f_x, f_u, V_x, V_xx):
@@ -86,30 +91,31 @@ class ILQRPlanner():
         K = -Q_uu_inv.dot(Q_ux)
         return k, K
 
-    def _get_derivatives_ilqr_quadratic_cost(self, x, u, Q, Qf, R):
-        fx = self.func_fx(x, u)
-        fu = self.func_fu(x, u)
-        lx = x.T@Q
-        lu = u.T@R
-        lxx = Q
-        lux = np.zeros((x.shape(0), u.shape(1)))
-        luu = R
+    def _get_derivatives_ilqr(self, x, u):
+        fx = self.fx(x, u)
+        fu = self.fu(x, u)
+        lx = self.lx(x, u)
+        lu = self.lu(x, u)
+        lxx = self.lxx(x, u)
+        lux = self.lux(x, u)
+        luu = self.luu(x, u)
 
         return lx, lu, lxx, lux, luu, fx, fu
 
-    def plan_ilqr_trajectory_quadratic_cost(self, x0, Q, Qf, R, N, max_iter=50, regu_init=100):
+    def _ilqr_expected_cost_reduction(self, Q_u, Q_uu, k):
+        return -Q_u.T.dot(k) - 0.5 * k.T.dot(Q_uu.dot(k))
+
+    def plan_ilqr_trajectory(self, x0, N, max_iter=50, regu_init=100):
         # First forward rollout
-        # First forward rollout
-        u_trj = np.random.randn(N-1, n_u)*0.0001
-        x_trj = rollout(x0, u_trj)
-        total_cost = cost_trj(x_trj, u_trj)
+        u_trj = np.random.randn(N-1, self.nu)*0.0001
+        x_trj = self._ilqr_rollout(x0, u_trj)
+        total_cost = self._ilqr_cost_trj(x_trj, u_trj)
         regu = regu_init
         max_regu = 10000
         min_regu = 0.01
 
         # Setup traces
         cost_trace = [total_cost]
-        expected_cost_redu_trace = []
         redu_ratio_trace = [1]
         redu_trace = []
         regu_trace = [regu]
@@ -117,12 +123,12 @@ class ILQRPlanner():
         # Run main loop
         for it in range(max_iter):
             # Backward and forward pass
-            k_trj, K_trj, expected_cost_redu = self._ilqr_backward_pass_quadratic_cost(
-                x_trj, u_trj, Q, Qf, R, regu)
+            k_trj, K_trj, expected_cost_redu = self._ilqr_backward_pass(
+                x_trj, u_trj, regu)
             x_trj_new, u_trj_new = self._ilqr_forward_pass(
                 x_trj, u_trj, k_trj, K_trj)
             # Evaluate new trajectory
-            total_cost = cost_trj(x_trj_new, u_trj_new)
+            total_cost = self._ilqr_cost_trj(x_trj_new, u_trj_new)
             cost_redu = cost_trace[-1] - total_cost
             redu_ratio = cost_redu / abs(expected_cost_redu)
             # Accept or reject iteration
@@ -147,3 +153,86 @@ class ILQRPlanner():
                 break
 
         return x_trj, u_trj, cost_trace, regu_trace, redu_ratio_trace, redu_trace
+
+
+if __name__ == "__main__":
+    from sympy import Symbol, Function, Matrix, MatrixSymbol
+    from sympy import sin, cos
+    from sympy import pprint, init_printing
+    from sympy import simplify, lambdify
+
+    import constants
+
+    init_printing()
+
+    t = Symbol('t')
+
+    m = Symbol('m')
+    g = Symbol('g')
+    rho = Symbol('rho')  # Air density
+    S = Symbol('S')  # Reference area (m^2)
+    Cltrim = Symbol('Clt')
+    Cdtrim = Symbol('Cdt')
+    dCl = Symbol('dCl')
+    dCd = Symbol('dCd')
+
+    const_dict = {g: constants.g, m: constants.m, rho: constants.rho, Cltrim: constants.Cltrim,
+                  Cdtrim: constants.Cdtrim, dCl: constants.delCl, dCd: constants.delCd, S: constants.S}
+
+    v = Function('v')(t)
+    psi = Function('psi')(t)
+    gamma = Function('gamma')(t)
+    x = Function('x')(t)
+    y = Function('y')(t)
+    h = Function('h')(t)
+
+    sigma = Function('sigma')(t)
+    epsilon = Function('epsilon')(t)
+
+    W = m * g
+    L = 1/2 * rho * S * v**2 * (Cltrim + dCl * epsilon)
+    D = 1/2 * rho * S * v**2 * (Cdtrim + dCd * epsilon)
+
+    vdot = -(D + W*sin(gamma)) / m
+    gammadot = (L * cos(sigma) - W * cos(gamma)) / (m*v)
+    psidot = L*sin(sigma) / (m*v*cos(gamma))
+    xdot = v*cos(gamma)*cos(psi)
+    ydot = v*cos(gamma)*sin(psi)
+    hdot = v*sin(gamma)
+
+    # Calculate the derivatives
+    xs = Matrix([v, gamma, psi, x, y, h])
+    u = Matrix([sigma, epsilon])
+    f = Matrix([vdot, gammadot, psidot, xdot, ydot, hdot])
+
+    fd = lambdify((xs.tolist(), u.tolist()),
+                  f.subs(const_dict), modules='numpy')
+    fx = lambdify((xs.tolist(), u.tolist()), f.jacobian(
+        xs).subs(const_dict), modules='numpy')
+    fu = lambdify((xs.tolist(), u.tolist()), f.jacobian(
+        u).subs(const_dict), modules='numpy')
+
+    x0 = np.array([[2, -pi/6, 0, 0, 0, 100, 0, 0]])
+
+    xr = np.array([[5, 0, -0.5, 0, 0, 0]]).T
+    Q = np.diag([1, 1, 1, 10, 10, 10])
+    R = np.diag([1, 2])
+    Qf = np.diag([0.1, 0.1, 0.1, 10, 10, 10])
+
+    def ls(x, u): return (x.T - xr.T).dot(Q).dot(x.T - xr.T) + u.T.dot(R).dot(u)
+
+    def lf(x, U): return (x.T - xr.T).dot(Qf).dot(x.T - xr.T)
+
+    def lx(x, u): return Q.dot(x - xr) + Q.T.dot(x - xr)
+
+    def lu(x, u): return R.dot(u) + R.T.dot(u)
+
+    def lux(x, u): return np.zeros((u.shape[0], x.shape[1]))
+
+    def lxx(x, u): return Q + Q.T
+
+    def luu(x, u): return R + R.T
+
+    ilqrp = ILQRPlanner(6, 2, f, ls, lf, lx, lu, lxx, lux, luu, fx, fu, 0.1)
+
+    ilqrp.plan_ilqr_trajectory(x0, 1000)
