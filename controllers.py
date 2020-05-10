db13cd5a3ae5d6e442116db3a18f4745595dfdb9
Josef <jxb@mit.edu>
2020-05-10 15:59:56 -0400

    lyapunov controller for dubins

--- controllers.py
+++ controllers.py
@@ -1,3 +1,6 @@
+from math import sin, cos, e
+import numpy as np
+
 def constant_controller(x):
     """Dummy controller function. Returns zero commands for both inputs to the parafoil. Ordinarily you would do some calculations with x to produce the commands u.
 
@@ -27,3 +30,25 @@ def minimum_time_controller(x, a, b):
         u = b
 
     return [u, 0]
+
+
+def dubins_lyapunov_controller(x, a, eps, umax):
+    assert eps > 0, 'Epsilon must be a positive real number'
+    assert (-umax <= a) & (a < umax), 'a must satisfy the following: -umax <= a < umax'
+
+    px = x[3]
+    py = x[4]
+    psi = x[2]
+
+    p = np.array([[px, py]]).T
+    R = np.array([[cos(psi), sin(psi)], [-sin(psi), cos(psi)]])
+    ptild = R@p
+    xbar = ptild[0, 0]
+
+    if xbar <= -eps:
+        k = a
+    elif xbar >= 0:
+        k = umax
+    else:
+        k = (umax - 1)/(1 + e**(1/(xbar + eps) + 1/xbar)) + a
+    return [k, 0]
--- run.py
+++ run.py
@@ -11,7 +11,7 @@ from graphics import *
 
 def ivp_wrapper(t, x):
     # Calculate control
-    u = constant_controller(x)
+    u = dubins_lyapunov_controller(x, 0.1, 8, 0.6)
     # Return just xdot
     return simulation_dynamics(x, u)
 
@@ -20,11 +20,11 @@ def ivp_wrapper(t, x):
 # 2 m/s speed, -pi/6 flight path angle, 100m in the air - everything else zero
 
 # Create time points we want data at...
-t = np.linspace(0, 30, 100)
+t = np.linspace(0, 100, 100)
 
 # Call odeint
-solution = solve_ivp(ivp_wrapper, (0, 30), [
-                     7, 0, 0, 30, 20, 100, 0, 0], t_eval=t)
+solution = solve_ivp(ivp_wrapper, (0, 100), [
+                     7, 0, 0, -70, 0, 100, 0, 0], t_eval=t)
 
 # Graphs solution
 visualizer = Visualizer(x_traj=solution.y, plot_heading=False)
