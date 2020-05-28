2b9534b36402f70047839f7ecc14f147694a457a
Josef <jxb@mit.edu>
2020-05-28 17:47:47 -0400

    Good set of params for lyapunov controller

--- controllers.py
+++ controllers.py
@@ -1,5 +1,7 @@
-from math import sin, cos, e
+from math import sin, cos, e, atan2, exp
+from constants import g
 import numpy as np
+
 
 def constant_controller(x):
     """Dummy controller function. Returns zero commands for both inputs to the parafoil. Ordinarily you would do some calculations with x to produce the commands u.
@@ -13,32 +15,18 @@ def constant_controller(x):
     return [0.1, 0.1]
 
 
-def minimum_time_controller(x, a, b):
-    """Implemented from Rademarcher (2009) section 4.2. Uses simplified dynamics to solve for minimum time trajectories consisting of straight lines over ground and maximum turn rate circular arcs.
-
-    Arguments:
-        x {[type]} -- [description]
-    """
-
-    p_psi = 0
-
-    if p_psi == 0:
-        u = 0
-    elif p_psi > 0:
-        u = a
-    else:
-        u = b
-
-    return [u, 0]
-
-
 def dubins_lyapunov_controller(x, a, eps, umax):
-    assert eps > 0, 'Epsilon must be a positive real number'
-    assert (-umax <= a) & (a < umax), 'a must satisfy the following: -umax <= a < umax'
-
+    '''
+    V = x[0]
+    gam = x[1]
+    '''
+    psi = x[2]
     px = x[3]
     py = x[4]
-    psi = x[2]
+
+    assert eps > 0, 'Epsilon must be a positive real number'
+    assert ((-umax <= a) & (a < umax)
+            ), 'a must satisfy the following: -umax <= a < umax'
 
     p = np.array([[px, py]]).T
     R = np.array([[cos(psi), sin(psi)], [-sin(psi), cos(psi)]])
@@ -50,5 +38,12 @@ def dubins_lyapunov_controller(x, a, eps, umax):
     elif xbar >= 0:
         k = umax
     else:
-        k = (umax - 1)/(1 + e**(1/(xbar + eps) + 1/xbar)) + a
+        n = (umax - a)
+        expnt = 1/(xbar+eps) + 1/xbar
+        d = 1 + exp(expnt)
+        k = n/d + a
+
+    # Convert dubins command to pseduo bank angle
+    # r = 1/(k*cos(gam))
+    # sig = atan2(V**2 * cos(gam), g*r)
     return [k, 0]
--- run.py
+++ run.py
@@ -1,17 +1,26 @@
 from dynamics import simulation_dynamics
-from controllers import *
+from controllers import dubins_lyapunov_controller
 from scipy.integrate import solve_ivp
-from math import pi
-
 import numpy as np
-from matplotlib import pyplot as plt
+from graphics import Visualizer
+from math import cos, tan
 
-from graphics import *
+# NOTE sigma=0.5 seems to give a turn radius of about 10m while not effecting gamma much
+# NOTE sigma=0.3 gives about a 15m radius
 
-
-def ivp_wrapper(t, x):
+def integrand(t, x):
+    #gam = x[1]
+    #print(gam)
+    #rmin = 10
+    #umax = 1/(rmin*cos(gam))
+    #umax = 0.1
+    # print(-1/(tan(gam)))
     # Calculate control
-    u = dubins_lyapunov_controller(x, 0.1, 8, 0.6)
+    umax = 0.3
+    a = 0.1
+    eps = 2
+    u = dubins_lyapunov_controller(x, a, eps, umax)
+    
     # Return just xdot
     return simulation_dynamics(x, u)
 
@@ -20,11 +29,12 @@ def ivp_wrapper(t, x):
 # 2 m/s speed, -pi/6 flight path angle, 100m in the air - everything else zero
 
 # Create time points we want data at...
-t = np.linspace(0, 100, 100)
+tf = 200
+t = np.linspace(0, tf, tf)
 
 # Call odeint
-solution = solve_ivp(ivp_wrapper, (0, 100), [
-                     7, 0, 0, -70, 0, 100, 0, 0], t_eval=t)
+solution = solve_ivp(integrand, (0, tf), [
+                     7, -0.15, 0.01, -1000, 1000, 9000, 0, 0], t_eval=t)
 
 # Graphs solution
 visualizer = Visualizer(x_traj=solution.y, plot_heading=False)
