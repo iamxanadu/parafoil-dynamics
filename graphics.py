0cd57c4277044df1e6e1142fc334b45c9c4de4f3
Josef <jxb@mit.edu>
2020-05-29 22:19:57 -0400

    Working Lyapunov test

--- graphics.py
+++ graphics.py
@@ -1,40 +1,67 @@
-from mpl_toolkits import mplot3d
 import matplotlib.pyplot as plt
 from matplotlib.patches import FancyArrowPatch
 from mpl_toolkits.mplot3d import proj3d
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-from matplotlib.animation import FuncAnimation
 from celluloid import Camera
 import numpy as np
+import logging as lg
+from math import pi
+
+lg.basicConfig(level=lg.INFO)
 
 
 class Arrow3D(FancyArrowPatch):
     def __init__(self, xs, ys, zs, *args, **kwargs):
-        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        FancyArrowPatch.__init__(self, (0, 0), (0, 0), *args, **kwargs)
         self._verts3d = xs, ys, zs
 
     def draw(self, renderer):
         xs3d, ys3d, zs3d = self._verts3d
         xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
-        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
         FancyArrowPatch.draw(self, renderer)
 
 
 class Visualizer(object):
-    def __init__(self, x_traj=None, t_delta=None, lyapunov_function=None, plot_heading=True, goal_pos=None, target_trajectory=None, render_animation=False, anim_path="anim.gif", ):
+
+    def __init__(self, t: np.ndarray, x_traj: np.ndarray, u_traj=None, max_size_no_subsample=500, subsample_divisor=100, lyapunov_function=None, plot_heading=True, goal_pos=None, target_trajectory=None, render_animation=False, anim_path="anim.gif", ):
         self.plot_heading = plot_heading
         self.goal_pos = goal_pos
         self.anim_path = anim_path
-        self.x_traj = x_traj
-        self.t_delta = t_delta
+
+        n = x_traj.shape[-1]
+        if n > max_size_no_subsample:
+            every = n // subsample_divisor
+            lg.info(
+                f"Trajectory is too large to display effectively. Subsampling every {every}")
+        else:
+            every = 1
+
+        self.time = t[::every]
+        self.V = x_traj[0, ::every]
+        self.gamma = x_traj[1, ::every]
+        self.psi = x_traj[2, ::every]
+        self.x_pos = x_traj[3, ::every]
+        self.y_pos = x_traj[4, ::every]
+        self.h = x_traj[5, ::every]
+        self.sigma = x_traj[6, ::every]
+        self.epsilon = x_traj[7, ::every]
+
+        if u_traj is not None:
+            self.com_sigma = u_traj[0, ::every]
+            self.com_epsilon = u_traj[1, ::every]
+        else:
+            self.com_sigma = None
+            self.com_epsilon = None
 
         if not render_animation:
-            if x_traj is not None:
-                self.plot_state_trajectory(x_traj, target_trajectory=target_trajectory)
+            self.plot_state_trajectory()
+            '''
+            TODO Implement after wind model done
             elif lyapunov_function is not None:
                 self.plot_landing_zone(lyapunov_function)
+            '''
         else:
-            time, V, gamma, psi, x_pos, y_pos, h, sigma, eta = self.process_state(self.x_traj, simplify_divisor=200)
 
             fig = plt.figure()
             camera = Camera(fig)
@@ -45,22 +72,26 @@ class Visualizer(object):
             ax0.set_zlabel('Altitude')
 
             # plots two points to enforce isometric grid
-            x_min = np.amin(x_pos)
-            y_min = np.amin(y_pos)
-            h_min = np.amin(h)
-            x_max = np.amax(x_pos)
-            y_max = np.amin(y_pos)
-            h_max = np.amin(h)
+            x_min = np.amin(self.x_pos)
+            y_min = np.amin(self.y_pos)
+            h_min = np.amin(self.h)
+            x_max = np.amax(self.x_pos)
+            y_max = np.amin(self.y_pos)
+            h_max = np.amin(self.h)
 
-            center = np.array([np.sum(x_pos)/x_pos.shape[0], np.sum(y_pos)/y_pos.shape[0], np.sum(h)/h.shape[0]])
+            center = np.array([np.sum(self.x_pos)/self.x_pos.shape[0],
+                               np.sum(self.y_pos)/self.y_pos.shape[0], np.sum(self.h)/self.h.shape[0]])
             width = np.amax([x_max - x_min, y_max - y_min, h_max - h_min])
             bounds = np.stack([center - width/2, center + width/2], axis=0)
-            ax0.scatter(bounds[:, 0], bounds[:, 1], bounds[:, 2], marker='.', color='white')
+            ax0.scatter(bounds[:, 0], bounds[:, 1],
+                        bounds[:, 2], marker='.', color='white')
 
             # renders animation frames
-            parafoil_scale = ((np.amax(x_pos) - np.amin(x_pos)) + (np.amax(y_pos) - np.amin(y_pos))) / 4
-            for i in range(V.shape[-1]):
-                self.animate(i, ax0, camera, parafoil_scale, V, gamma, psi, x_pos, y_pos, h, sigma, eta, target_trajectory)
+            parafoil_scale = ((np.amax(self.x_pos) - np.amin(self.x_pos)) +
+                              (np.amax(self.y_pos) - np.amin(self.y_pos))) / 4
+            for i in range(self.V.shape[-1]):
+                self.animate(i, ax0, camera, parafoil_scale, self.V, self.gamma,
+                             self.psi, self.x_pos, self.y_pos, self.h, self.sigma, self.epsilon, target_trajectory)
 
             anim = camera.animate()
             anim.save(self.anim_path, writer='imagemagick', fps=30)
@@ -68,7 +99,7 @@ class Visualizer(object):
     ### private methods ###
 
     # animation iteration
-    def animate(self, i, ax, camera, parafoil_scale, V, gamma, psi, x_pos, y_pos, h, sigma, eta, target_trajectory=None):
+    def animate(self, i, ax, camera, parafoil_scale, V, gamma, psi, x_pos, y_pos, h, sigma, epsilon, target_trajectory=None):
 
         # adds parafoil model to render
         parafoil_width = 0.3 * 0.5 * parafoil_scale
@@ -90,8 +121,10 @@ class Visualizer(object):
             [1.0, 0.0, 0.0],
         ]).transpose()
         transformation_matrix = np.array([
-            [np.cos(alpha)*np.cos(beta), -np.sin(alpha), np.cos(alpha)*np.sin(beta)],
-            [np.sin(alpha)*np.cos(beta), np.cos(alpha), np.sin(alpha)*np.sin(beta)],
+            [np.cos(alpha)*np.cos(beta), -np.sin(alpha),
+             np.cos(alpha)*np.sin(beta)],
+            [np.sin(alpha)*np.cos(beta), np.cos(alpha),
+             np.sin(alpha)*np.sin(beta)],
             [-np.sin(beta), 0, np.cos(beta)],
         ])
         new_offsets = transformation_matrix.dot(points)
@@ -100,94 +133,64 @@ class Visualizer(object):
         hs = new_offsets[2, :] + height
 
         verts = [list(zip(xs, ys, hs))]
-        collection = Poly3DCollection(verts, linewidths=1, edgecolors='red', alpha=0.2, zsort='min')
+        collection = Poly3DCollection(
+            verts, linewidths=1, edgecolors='red', alpha=0.2, zsort='min')
         face_color = "salmon"
         collection.set_facecolor(face_color)
         ax.add_collection3d(collection)
 
-        # also plots target trajectory if necessary
-        if target_trajectory is not None:
-            time_b, V_b, gamma_b, psi_b, x_pos_b, y_pos_b, h_b, sigma_b, eta_b = self.process_state(target_trajectory)
-            ax.plot3D(x_pos_b, y_pos_b, h_b, 'black')
-
         # saves frame
         camera.snap()
-        print(" * Finished rendering frame "+str(i))
-
-    # converts state trajectory into separate time-indexed state variables
-    def process_state(self, x_traj, simplify_divisor=75):
-        if x_traj.shape[-1] > 500:
-            print(" !! Trajectory is too large for rendering, simplifying rendered trajectory...")
-            x_traj = x_traj[:, ::x_traj.shape[-1] // simplify_divisor]
-
-        time = np.arange(x_traj.shape[-1])
-        V = x_traj[0, :]
-        gamma = x_traj[1, :]
-        psi = x_traj[2, :]
-        x_pos = x_traj[3, :]
-        y_pos = x_traj[4, :]
-        h = x_traj[5, :]
-        sigma = x_traj[6, :]
-        eta = x_traj[7, :]
-        return (time, V, gamma, psi, x_pos, y_pos, h, sigma, eta)
+        lg.info(f" * Finished rendering frame {i}")
 
     ### public methods ###
 
     # plots the state of the parafoil/rocket over time
-    def plot_state_trajectory(self, x_traj, target_trajectory=None):
-        time, V, gamma, psi, x_pos, y_pos, h, sigma, eta = self.process_state(x_traj)
-        dilution_factor = x_traj.shape[-1] / time.shape[0]
+    def plot_state_trajectory(self, target_trajectory=None):
 
-        fig = plt.figure(figsize=plt.figaspect(1.5))
+        fig = plt.figure(figsize=plt.figaspect(1))
         fig.suptitle('State Trajectory')
 
-        ax0 = fig.add_subplot(2, 1, 1, projection='3d')
+        ax0 = fig.add_subplot(2, 2, 1, projection='3d')
         ax0.set_xlabel('X')
         ax0.set_ylabel('Y')
         ax0.set_zlabel('Altitude')
-        ax0.plot3D(x_pos, y_pos, h, 'blue')
-        ax0.scatter(x_pos, y_pos, h, marker='o')
+        ax0.plot3D(self.x_pos, self.y_pos, self.h, 'blue')
+        ax0.scatter(self.x_pos, self.y_pos, self.h, marker='o')
 
         # plots two points to enforce isometric grid
-        x_min = np.amin(x_pos)
-        y_min = np.amin(y_pos)
-        h_min = np.amin(h)
-        x_max = np.amax(x_pos)
-        y_max = np.amin(y_pos)
-        h_max = np.amin(h)
+        x_min = np.amin(self.x_pos)
+        y_min = np.amin(self.y_pos)
+        h_min = np.amin(self.h)
+        x_max = np.amax(self.x_pos)
+        y_max = np.amin(self.y_pos)
+        h_max = np.amin(self.h)
 
-        center = np.array([np.sum(x_pos)/x_pos.shape[0], np.sum(y_pos)/y_pos.shape[0], np.sum(h)/h.shape[0]])
+        center = np.array([np.sum(self.x_pos)/self.x_pos.shape[0],
+                           np.sum(self.y_pos)/self.y_pos.shape[0], np.sum(self.h)/self.h.shape[0]])
         width = np.amax([x_max - x_min, y_max - y_min, h_max - h_min])
         bounds = np.stack([center - width/2, center + width/2], axis=0)
-        ax0.scatter(bounds[:, 0], bounds[:, 1], bounds[:, 2], marker='.', color='white')
+        ax0.scatter(bounds[:, 0], bounds[:, 1],
+                    bounds[:, 2], marker='.', color='white')
 
-        # marks points near goal state
-        if self.goal_pos is not None:
-            diffs = np.zeros((3, V.shape[-1]))
-            diffs[0, :] = x_pos.astype(float)
-            diffs[1, :] = y_pos.astype(float)
-            diffs[2, :] = h.astype(float)
-            goals = np.repeat(self.goal_pos, V.shape[-1], axis=1).astype(float)
-            dist = np.sum(np.abs(diffs - goals) ** 2.0, axis=0) ** (1./2.)
-            
-            best_step = np.argmin(dist)
-            ax0.scatter(x_pos[best_step], y_pos[best_step], h[best_step], c='g', s=200)
-            ax0.scatter(float(self.goal_pos[0]), float(self.goal_pos[1]), float(self.goal_pos[2]), c='orange', s=140)
+        # TODO Add support for graphing a goal manifold
 
         # plots vectors
-        parafoil_scale = ((np.amax(x_pos) - np.amin(x_pos)) + (np.amax(y_pos) - np.amin(y_pos))) / 4
-        for i in range(V.shape[-1]):
+        parafoil_scale = ((np.amax(self.x_pos) - np.amin(self.x_pos)) +
+                          (np.amax(self.y_pos) - np.amin(self.y_pos))) / 4
+
+        for i in range(self.V.shape[-1]):
 
             # plots simple parafoil
             parafoil_width = 0.3 * 0.5 * parafoil_scale
             parafoil_depth = 0.15 * 0.5 * parafoil_scale
-            x = x_pos[i]
-            y = y_pos[i]
-            height = h[i]
+            x = self.x_pos[i]
+            y = self.y_pos[i]
+            height = self.h[i]
 
             # modifies xs, ys, and hs depending on gamma, psi, and sigma(?)
-            beta = gamma[i]
-            alpha = psi[i] + (np.pi)
+            beta = self.gamma[i]
+            alpha = self.psi[i] + (np.pi)
             points = np.array([
                 [-parafoil_depth, -parafoil_width, 0],
                 [-parafoil_depth, parafoil_width, 0],
@@ -198,8 +201,10 @@ class Visualizer(object):
                 [1.0, 0.0, 0.0],
             ]).transpose()
             transformation_matrix = np.array([
-                [np.cos(alpha)*np.cos(beta), -np.sin(alpha), np.cos(alpha)*np.sin(beta)],
-                [np.sin(alpha)*np.cos(beta), np.cos(alpha), np.sin(alpha)*np.sin(beta)],
+                [np.cos(alpha)*np.cos(beta), -np.sin(alpha),
+                 np.cos(alpha)*np.sin(beta)],
+                [np.sin(alpha)*np.cos(beta), np.cos(alpha),
+                 np.sin(alpha)*np.sin(beta)],
                 [-np.sin(beta), 0, np.cos(beta)],
             ])
             new_offsets = transformation_matrix.dot(points)
@@ -208,51 +213,50 @@ class Visualizer(object):
             hs = new_offsets[2, :] + height
 
             verts = [list(zip(xs, ys, hs))]
-            collection = Poly3DCollection(verts, linewidths=1, edgecolors='red', alpha=0.2, zsort='min')
+            collection = Poly3DCollection(
+                verts, linewidths=1, edgecolors='red', alpha=0.2, zsort='min')
             face_color = "salmon"
             collection.set_facecolor(face_color)
             ax0.add_collection3d(collection)
 
             # plots v
             if self.plot_heading:
-                v = V[i]
+                v = self.V[i]
                 new_heading = transformation_matrix.dot(heading)
-                a = Arrow3D([x, x + new_heading[0, 0] * v], [y, y + new_heading[1, 0] * v], 
-                    [height, height + new_heading[2, 0] * v], mutation_scale=10, 
-                    lw=2, arrowstyle="-|>", color="r")
+                a = Arrow3D([x, x + new_heading[0, 0] * v], [y, y + new_heading[1, 0] * v],
+                            [height, height + new_heading[2, 0] * v], mutation_scale=10,
+                            lw=2, arrowstyle="-|>", color="r")
                 ax0.add_artist(a)
-        
-        # plots sigma and eta
-        if self.t_delta is not None:
-            time = time * self.t_delta * dilution_factor
-        ax1 = fig.add_subplot(2, 1, 2)
-        ax1.plot(time, sigma, 'tab:orange')
-        ax1.plot(time, eta, 'tab:green')
-        ax1.legend(['Sigma', 'Eta'])
-        ax1.set_xlabel('Time (Seconds)')
 
-        # also plots target trajectory if necessary
-        if target_trajectory is not None:
-            time, V, gamma, psi, x_pos, y_pos, h, sigma, eta = self.process_state(target_trajectory)
-            ax0.plot3D(x_pos, y_pos, h, 'black')
+        # plots sigma and eta
+        ax1 = fig.add_subplot(2, 2, 2)
+        ax1.plot(self.time, self.sigma, 'tab:orange')
+        ax1.plot(self.time, self.epsilon, 'tab:green')
+        ax1.legend(['Sigma', 'Epsilon'])
+        ax1.set_xlabel('Time (s)')
+
+        if self.com_sigma is not None and self.com_epsilon is not None:
+            ax1.plot(self.time, self.com_sigma, 'tab:cyan')
+            ax1.plot(self.time, self.com_epsilon, 'tab:red')
+            ax1.legend(
+                ['Sigma', 'Epsilon', 'Commanded Sigma', 'Commanded Epsilon'])
+
+        ax2 = fig.add_subplot(2, 2, 3)
+        ax2.plot(self.time, self.V, 'tab:orange')
+        ax2.set_xlabel('Time (s)')
+        ax2.set_ylabel('m/s')
+        ax2.set_title('Velocity')
+        ax2.legend(['Velocity'])
+
+        ax3 = fig.add_subplot(2, 2, 4)
+        ax3.plot(self.time, self.gamma, 'tab:cyan')
+        ax3.set_xlabel('Time (s)')
+        ax3.set_ylabel('rad')
+        ax3.set_title('Flight Path Angle')
+        ax3.legend(['Flight Path Angle'])
 
         plt.show()
 
     # samples lyapunov function to approximate range of landing zone
     def plot_landing_zone(self, lyapunov_control_function):
-        pass
-
-
-# unit tests when running graphics.py directly
-if __name__ == "__main__":
-
-    test_x_traj = np.zeros((8, 30))
-    test_x_traj[0] = (np.sin(np.arange(30) / 1.6) * 0.15) + 0.2
-    test_x_traj[1] = (1.0 - np.arange(30) / 29.0) * 0.5
-    test_x_traj[2] = np.arange(30) * np.pi * 4.5 / 29.0
-    test_x_traj[3] = np.cos(np.arange(30) / 2.0)
-    test_x_traj[4] = np.sin(np.arange(30) / 2.1)
-    test_x_traj[5] = 1.0 - (np.arange(30) / 29.0)
-    test_x_traj[6] = np.sin(np.arange(30) / 4.0)
-    test_x_traj[7] = np.cos(np.arange(30) / 4.0)
-    visualizer = Visualizer(x_traj=test_x_traj)
+        raise NotImplementedError
--- run.py
+++ run.py
@@ -3,6 +3,9 @@ from controllers import dubins_lyapunov_controller
 from scipy.integrate import solve_ivp
 import numpy as np
 from graphics import Visualizer
+import logging as lg
+
+lg.basicConfig(level=lg.INFO)
 
 # NOTE sigma=0.5 seems to give a turn radius of about 10m while not effecting gamma much
 # NOTE sigma=0.3 gives about a 15m radius
@@ -17,12 +20,17 @@ def integrand(t, x):
     # Return just xdot
     return simulation_dynamics(t, x, u)
 
-
-tf = 200
-t = np.linspace(0, tf, tf)
+# Solve until ground hit
+# hf = sys.float_info.max
+# final_height_threshold = 50
 x0 = [7, -0.15, 0.01, -1000, 1000, 9000, 0, 0]
+tf = 1000
+t = np.linspace(0, tf, tf)
+
 # Call odeint
 solution = solve_ivp(integrand, (0, tf), x0, t_eval=t)
 
+lg.info(f'Solver finished with message: {solution.message}')
+
 # Graphs solution
-visualizer = Visualizer(x_traj=solution.y, plot_heading=False)
+visualizer = Visualizer(solution.t, solution.y, render_animation=False, subsample_divisor=500)
